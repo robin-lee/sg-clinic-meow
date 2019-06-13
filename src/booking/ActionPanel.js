4fecee3643c36fc4916cb6ce6c173893b10d683a
jnoorul <noorul.it@gmail.com>
2019-06-13 17:56:26 +0800

    removing all pwa related features from cra

--- src/booking/ActionPanel.js
+++ src/booking/ActionPanel.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import {Button} from "../common/Atoms";
 
 export default function ActionPanel(props) {
-  const btnStyle = {width: '100%', backgroundColor: '#FF5A5F', height: '48px'};
   return (
     <div style={{marginTop: '2.5rem'}}>
       <Link to="/apt-review">
