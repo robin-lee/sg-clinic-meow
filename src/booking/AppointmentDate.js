2a42acbdb1d68fb4e95d8d16e9b4b1ff73ddfd9d
jnoorul <noorul.it@gmail.com>
2019-06-15 12:23:04 +0800

    from antd datepicker to airbnb datepicker

--- src/booking/AppointmentDate.js
+++ src/booking/AppointmentDate.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import {DatePicker} from 'antd';
+import { SingleDatePicker } from 'react-dates';
+import * as moment from 'moment';
 import {ItemContainer, Label} from '../common/Typography';
 
-export default function AppointmentDate(props) {
-  return (
-    <ItemContainer>
-      <Label>DATE</Label>
-      <DatePicker size="large" style={{width: '100%'}}/>
-    </ItemContainer>
-  );
+export default class AppointmentDate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: moment(), focused: false};
+    this.dateSelected = this.dateSelected.bind(this);
+    this.focusChanged = this.focusChanged.bind(this);
+  }
+
+  dateSelected(date) {
+    this.setState({ ...this.state, date });
+  }
+
+  focusChanged({focused}) {
+    this.setState({ ...this.state, focused })
+  }
+
+  render() {
+    return (
+      <ItemContainer>
+        <Label>DATE</Label>
+        <SingleDatePicker
+          date={this.state.date}
+          onDateChange={this.dateSelected}
+          focused={this.state.focused}
+          onFocusChange={this.focusChanged}
+          small
+          numberOfMonths={1}
+          readOnly={true}
+          block={true}
+          openDirection="up"
+          id="appointmentDate"
+          style={{width: '100%' }}
+        />
+      </ItemContainer>
+    );
+  }
 }
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'antd/dist/antd.css';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
