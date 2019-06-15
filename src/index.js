9ed030be2ac9485a6ca0c9f931ed3467faa6690a
jnoorul <noorul.it@gmail.com>
2019-06-15 14:09:46 +0800

    customize style for react-dates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+import './styles/react_dates_overrides.css';
 import './index.css';
 import App from './App';
 
