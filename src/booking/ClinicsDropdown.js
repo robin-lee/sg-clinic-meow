84dc90a50d536de2b4623d709930db1e517b77fb
jnoorul <noorul.it@gmail.com>
2019-06-15 14:34:00 +0800

    dropdown selection

--- src/booking/ClinicsDropdown.js
+++ src/booking/ClinicsDropdown.js
@@ -3,33 +3,41 @@ import {Button, Dropdown, Menu, Icon} from 'antd';
 import {ItemContainer, Label} from '../common/Typography';
 
 
-const menu = (
-  <Menu onClick={() => {
-  }}>
-    <Menu.Item key="1">
-      <Icon type="user"/>
-      Dermatology Practice, Novena
-    </Menu.Item>
-    <Menu.Item key="2">
-      <Icon type="user"/>
-      My Family Practice, Sengkang
-    </Menu.Item>
-    <Menu.Item key="3">
-      <Icon type="user"/>
-      Child Speciality Clinic, Tampines
-    </Menu.Item>
-  </Menu>
-);
-
-export default function ClinicsDropdown(props) {
-  return (
-    <ItemContainer>
-      <Label>CLINIC</Label>
-      <Dropdown overlay={menu} trigger={["click"]} >
-        <Button style={{width: '100%', height: '40px'}}>
-          Select Clinic <Icon type="down"/>
-        </Button>
-      </Dropdown>
-    </ItemContainer>
-  );
+export default class ClinicsDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {clinic: 'Select Clinic'};
+    this.onClinicSelected = this.onClinicSelected.bind(this);
+  }
+  onClinicSelected(selectedItem) {
+    this.setState({...this.state, clinic: selectedItem.domEvent.target.innerText});
+  }
+  render() {
+    const menu = (
+      <Menu onClick={this.onClinicSelected}>
+        <Menu.Item key="1">
+          <Icon type="user"/>
+          Dermatology Practice, Novena
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="user"/>
+          My Family Practice, Sengkang
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="user"/>
+          Child Speciality Clinic, Tampines
+        </Menu.Item>
+      </Menu>
+    );
+    return (
+      <ItemContainer>
+        <Label>CLINIC</Label>
+        <Dropdown overlay={menu} trigger={["click"]} >
+          <Button style={{width: '100%', height: '40px'}}>
+            {this.state.clinic} <Icon type="down"/>
+          </Button>
+        </Dropdown>
+      </ItemContainer>
+    );
+  }
 }
--- src/booking/DoctorsDropdown.js
+++ src/booking/DoctorsDropdown.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import {Button, Dropdown, Menu, Icon} from 'antd';
-import {ItemContainer, Label} from "../common/Typography";
+import {ItemContainer, Label} from '../common/Typography';
 
-const menu = (
-  <Menu onClick={() => {
-  }}>
-    <Menu.Item key="1">
-      <Icon type="user"/>
-      Dr Lim
-    </Menu.Item>
-    <Menu.Item key="2">
-      <Icon type="user"/>
-      Dr Tony
-    </Menu.Item>
-    <Menu.Item key="3">
-      <Icon type="user"/>
-      Dr Ram
-    </Menu.Item>
-  </Menu>
-);
 
-export default function DoctorsDropdown(props) {
-  return (
-    <ItemContainer>
-      <Label>DOCTOR</Label>
-      <Dropdown overlay={menu} trigger={["click"]} >
-        <Button style={{width: '100%', height: '40px'}}>
-          Select Your Doctor <Icon type="down"/>
-        </Button>
-      </Dropdown>
-    </ItemContainer>
-  );
+export default class DoctorsDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {doctor: 'Select Your Doctor'};
+    this.onDoctorSelected = this.onDoctorSelected.bind(this);
+  }
+  onDoctorSelected(selectedItem) {
+    this.setState({...this.state, doctor: selectedItem.domEvent.target.innerText});
+  }
+  render() {
+    const menu = (
+      <Menu onClick={this.onDoctorSelected}>
+        <Menu.Item key="1">
+          <Icon type="user"/>
+          Dr Lim
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="user"/>
+          Dr Tony
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="user"/>
+          Dr Ram
+        </Menu.Item>
+      </Menu>
+    );
+    return (
+      <ItemContainer>
+        <Label>CLINIC</Label>
+        <Dropdown overlay={menu} trigger={["click"]} >
+          <Button style={{width: '100%', height: '40px'}}>
+            {this.state.doctor} <Icon type="down"/>
+          </Button>
+        </Dropdown>
+      </ItemContainer>
+    );
+  }
 }
